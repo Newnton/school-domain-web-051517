ed4d5b6b9cbaf337746febcdae633dea22c9aae8
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-16 17:20:22 -0400

    finished lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  attr_reader :roster
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = name.split("banana")
+    end
+  end
+  def grade(ui)
+    @roster[ui]
+  end
+  def sort
+    sorted = {}
+    @roster.each do |grade, names|
+      sorted[grade] = names.sort
+    end
+    sorted
+  end
+end
